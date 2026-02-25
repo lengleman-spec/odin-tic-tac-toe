4de9cbc25bd362baa219d6064873969a3f7737fe
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-25 11:09:59 -0500

    Add logic to check for a winner using any of the winning combinations.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // array inside a Gameboard object
-Gameboard = [
-  [1, 2, 3],
-  [1, 4, 7],
-  [3, 6, 9],
-  [7, 8, 9],
-  [2, 5, 8],
-  [1, 5, 9],
-  [3, 5, 7],
-];
+gameboard = [[""], [""], [""], [""], [""], [""], [""]];
 
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const player1 = createPlayer("Liz", "X");
 const player2 = createPlayer("Computer", "O");
 
 let currentPlayer = player1;
 
-// function to switch player
+// function for current player / to switch player
 function switchPlayer() {
   if (currentPlayer === player1) {
     currentPlayer = player2;
@@ -42,7 +44,37 @@ function createPlayer(name, marker) {
   };
 }
 
-// need function to play round
+let gameOver = false;
+
+// need function to play round:
+function playRound(index) {
+  if (gameOver) {
+    return;
+  }
+  // Check if there's spaces available:
+  if (gameboard[index] !== "") {
+    return;
+  }
+
+  // check to place marker:
+  gameboard[index] = currentPlayer.marker;
+}
+
+// check winner:
+// loops through winning patterns
+// returns true if any match
+function checkWinner() {
+  return winningCombos.some((combo) => {
+    const [a, b, c] = combo;
+
+    return (
+      gameboard[a] !== "" &&
+      gameboard[a] === gameboard[b] &&
+      gameboard[a] === gameboard[c]
+    );
+  });
+}
+
 // function to play 1 move
 
 // function to check for winner
